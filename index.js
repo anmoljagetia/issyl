bc102edbab58e4b02bce2e6f1c9ed4875797cb42
Anmol Jagetia <anmoljagetia@hotmail.com>
2015-04-11 04:43:16 +0530

    adding routes for 404 and 301

--- index.js
+++ index.js
@@ -47,6 +47,44 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+// Define submit route
+app.post('/', function (req, res) {
+    // Declare variables
+    var url, id;
+
+    // Get URL
+    url = req.body.url;
+
+    // Create a hashed short version
+    id = shortid.generate();
+
+    // Store them in Redis
+    client.set(id, url, function () {
+        // Display the response
+        res.render('output', { id: id, base_url: base_url });
+    });
+});
+
+// Define link route
+app.route('/:id').all(function (req, res) {
+    // Get ID and remove whitespaces
+    var id = req.params.id.trim();
+
+    // Look up the URL
+    client.get(id, function (err, reply) {
+        if (!err && reply) {
+            // Redirect user to it
+            res.status(301);
+            res.set('Location', reply);
+            res.send();
+        } else {
+            // Confirm no such link in database
+            res.status(404);
+            res.render('error');
+        }
+    });
+});
+
 // Serve static files
 app.use(express.static(__dirname + '/static'));
 
